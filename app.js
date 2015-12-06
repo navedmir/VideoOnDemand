07cd1c034692b6e6cd2ddd164e16537ee80def5e
navedmir <navedmir16@gmail.com>
2015-12-06 20:37:25 +0530

    Favicon

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var app = express();
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var routes = require('./controllers');
+var favicon = require('serve-favicon');
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
+
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({
